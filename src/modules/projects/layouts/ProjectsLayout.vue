00acc6a6c98cd803e77f14b63d06b21d8abe7484
Edgar Vergara <edgarvm13@hotmail.com>
2024-11-18 19:51:35 +0100

    ✨ components structure

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <!-- TOP MENU -->
+    <top-menu />
+
+    <main>
+      <!-- SIDE MENU -->
+      <side-menu />
+
+      <!-- ROUTER VIEW -->
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import ProjectsLayout from '@/modules/projects/layouts/ProjectsLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout,
+    },
+  ],
+});
 
-export default router
+export default router;
