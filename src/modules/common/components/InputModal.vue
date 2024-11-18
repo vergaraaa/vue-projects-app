3233efa1b612b0b0f2f6da3650b5b8d0e732db57
Edgar Vergara <edgarvm13@hotmail.com>
2024-11-18 20:38:03 +0100

    ✨ input modal

---
+++ src/modules/common/components/InputModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <dialog class="modal" :open="open">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p v-if="subtitle" class="py-4">{{ subtitle }}</p>
+
+      <div class="modal-action flex flex-col">
+        <form method="dialog" @submit.prevent="submitValue">
+          <input
+            ref="inputRef"
+            type="text"
+            v-model="inputValue"
+            :placeholder="placeholder ?? 'Project name'"
+            class="input input-bordered input-primary w-full flex-1"
+          />
+
+          <div class="flex justify-end mt-5 gap-4">
+            <button type="button" class="btn" @click="onClose">Close</button>
+
+            <button class="btn btn-primary">Accept</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </dialog>
+
+  <div
+    v-if="open"
+    class="modal-backdrop fixed top-0 left-0 z-10 bg-black/40 w-screen h-screen"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+interface Props {
+  open: boolean;
+  title: string;
+  subtitle?: string;
+  placeholder?: string;
+}
+
+defineProps<Props>();
+
+const emit = defineEmits<{
+  close: [void];
+  value: [text: string];
+}>();
+
+const inputValue = ref('');
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const onClose = () => {
+  inputValue.value = '';
+  emit('close');
+};
+
+const submitValue = () => {
+  if (!inputValue.value) {
+    inputRef.value?.focus();
+    return;
+  }
+
+  emit('value', inputValue.value.trim());
+  emit('close');
+
+  inputValue.value = '';
+};
+</script>
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import AddCircle from '@/modules/common/icons/AddCircle.vue';
-import FabButton from '@/modules/common/components/FabButton.vue';
-</script>
-
 <template>
   <div class="overflow-x-auto w-full">
     <table class="table">
@@ -25,8 +20,34 @@ import FabButton from '@/modules/common/components/FabButton.vue';
       </tbody>
     </table>
 
-    <fab-button @click="() => console.log('click')">
+    <input-modal
+      :open="modalOpen"
+      title="New project"
+      subtitle="Give a unique name to your project"
+      @close="onClose"
+      @value="onValue"
+    />
+
+    <fab-button @click="modalOpen = true">
       <add-circle />
     </fab-button>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
+
+const modalOpen = ref(false);
+
+const onClose = () => {
+  modalOpen.value = false;
+};
+
+const onValue = (projectName: string) => {
+  console.log({ projectName });
+};
+</script>
