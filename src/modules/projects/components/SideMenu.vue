9a124dfc61068592c7b0e79a402a7cbe7832fdbf
Edgar Vergara <edgarvm13@hotmail.com>
2024-11-20 16:19:17 +0100

    🩹 add router link

--- src/modules/projects/components/SideMenu.vue
+++ src/modules/projects/components/SideMenu.vue
@@ -1,6 +1,9 @@
 <template>
   <aside class="bg-base-200 w-72 min-h-screen">
-    <h2 class="text-lg font-bold mx-4">Projects</h2>
+    <RouterLink to="/">
+      <h2 class="text-lg font-bold mx-4">Projects</h2>
+    </RouterLink>
+
     <p v-if="projectsStore.noProjects" class="text-sm text-gray-500 mx-4">No projects</p>
 
     <ul v-else class="menu">
